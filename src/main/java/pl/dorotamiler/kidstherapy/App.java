90123faa40c3d8011874847b1618dee17dd04124
Dorota Miler <dorota.miler7@gmail.com>
2021-02-17 15:50:25 +0100

    javadoc describing application purpose

--- src/main/java/pl/dorotamiler/kidstherapy/App.java
+++ src/main/java/pl/dorotamiler/kidstherapy/App.java
@@ -1,5 +1,24 @@
 package pl.dorotamiler.kidstherapy;
 
+/**
+ * This application is providing solution in health
+ * protection and education, which main goal is to
+ * facilitate search of qualified specialists for
+ * children with development disorders such as:
+ *
+ * - delay in speech development
+ * - sensory processing disorders
+ * - multi-specialized comprehensive rehabilitation
+ *
+ * It is crucial to have means to find quick and
+ * adequate help in order to get the best shot at
+ * preparing children for an independent life in
+ * the community, apart from psychological assistance.
+ *
+ *
+ */
+
+
 
 public class App {
     public static void main( String[] args ) {
