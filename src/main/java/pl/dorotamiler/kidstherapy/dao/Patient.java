0346f68e18930c3e78335e9592c2da117fd0743a
Dorota Miler <dorota.miler7@gmail.com>
2021-02-18 12:10:34 +0100

    Patient class with adequate constructors in lines with User class

--- src/main/java/pl/dorotamiler/kidstherapy/dao/Patient.java
+++ src/main/java/pl/dorotamiler/kidstherapy/dao/Patient.java
@@ -1,6 +1,7 @@
 package pl.dorotamiler.kidstherapy.dao;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Patient extends User {
 
@@ -11,5 +12,33 @@ public class Patient extends User {
         this.address = address;
     }
 
+    public Patient(String pesel){
+        super(pesel);
+    }
 
+    public Address getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Patient{");
+        sb.append("address=").append(address);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Patient patient = (Patient) o;
+        return Objects.equals(address, patient.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), address);
+    }
 }
