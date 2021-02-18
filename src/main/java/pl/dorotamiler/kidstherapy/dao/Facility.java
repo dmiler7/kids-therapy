a9d56629b4514d741cc954a6cce0e18c2f7c82b0
Dorota Miler <dorota.miler7@gmail.com>
2021-02-18 11:29:31 +0100

    default constructor on Facility class

---
+++ src/main/java/pl/dorotamiler/kidstherapy/dao/Facility.java
@@ -0,0 +1,12 @@
+package pl.dorotamiler.kidstherapy.dao;
+
+public class Facility {
+    private String name;
+    private Address address;
+
+    public Facility() {
+        this.name = "";
+        this.address = new Address();
+    }
+
+}
