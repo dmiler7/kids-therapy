a3f9d94a50b07eae7b6748c4fa5f6e711e3bfa62
Dorota Miler <dorota.miler7@gmail.com>
2021-02-18 12:42:15 +0100

    modify toString() in Address for easier reading

--- src/main/java/pl/dorotamiler/kidstherapy/dao/Address.java
+++ src/main/java/pl/dorotamiler/kidstherapy/dao/Address.java
@@ -36,4 +36,14 @@ public class Address {
     public String getPhoneNumber() {
         return phoneNumber;
     }
+
+    @Override
+    public String toString() {
+        return "Address {" +
+                "Street : " + this.street +
+                ", City : " + this.city +
+                ", Postal Code : " + this.postalCode +
+                ", Phone Number : " + this.phoneNumber +
+                '}';
+    }
 }
