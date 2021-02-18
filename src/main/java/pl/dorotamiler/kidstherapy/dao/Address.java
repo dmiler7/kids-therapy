0a7fd4523bb536a7fd6d4c4ed65a90c44d43fba7
Dorota Miler <dorota.miler7@gmail.com>
2021-02-18 11:30:44 +0100

    Revert "default constructor"
    This reverts commit c8e4caa0bfe6a5e4b60eeae05ba18945449793fe.

--- src/main/java/pl/dorotamiler/kidstherapy/dao/Address.java
+++ src/main/java/pl/dorotamiler/kidstherapy/dao/Address.java
@@ -7,13 +7,6 @@ public class Address {
     private Integer postalCode;
     private String phoneNumber;
 
-    public Address() {
-        this.street = "";
-        this.city = "";
-        this.postalCode = null;
-        this.phoneNumber = "";
-    }
-
     public Address(String street, String city, Integer postalCode, String phoneNumber) {
         this.street = street;
         this.city = city;
