c8d6d0d3defb39da47e2c69f079692e5d9f00806
Dorota Miler <dorota.miler7@gmail.com>
2021-02-18 12:02:38 +0100

    change form Patient class to User class for versatility as abstract class will serve Pateint class next

--- src/main/java/pl/dorotamiler/kidstherapy/dao/User.java
+++ src/main/java/pl/dorotamiler/kidstherapy/dao/User.java
@@ -3,17 +3,28 @@ package pl.dorotamiler.kidstherapy.dao;
 import java.util.Date;
 import java.util.Objects;
 
-public class Patient {
+public abstract class User {
 
     private String name;
     private String lastName;
     private Date date;
-    private String PESEL;
-    private int age;
+    private String pesel;
 
-    public Patient(String name, String lastName){
+    public User(){
+    this.name="";
+    this.lastName="";
+    this.date=new Date();
+    this.pesel="";
+    }
+
+    public User(String pesel){
+        this.pesel=pesel;
+    }
+
+    public User(String name, String lastName, Date date){
         this.name = name;
         this.lastName=lastName;
+        this.date=date;
     }
 
     public String getName() {
@@ -28,22 +39,13 @@ public class Patient {
         return date;
     }
 
-    public String getPESEL() {
-        return PESEL;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
     @Override
     public String toString() {
-        final StringBuffer sb = new StringBuffer("Patient{");
+        final StringBuffer sb = new StringBuffer("User{");
         sb.append("name='").append(name).append('\'');
         sb.append(", lastName='").append(lastName).append('\'');
         sb.append(", date=").append(date);
-        sb.append(", PESEL='").append(PESEL).append('\'');
-        sb.append(", age=").append(age);
+        sb.append(", pesel='").append(pesel).append('\'');
         sb.append('}');
         return sb.toString();
     }
@@ -52,16 +54,13 @@ public class Patient {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Patient patient = (Patient) o;
-        return age == patient.age
-                && Objects.equals(name, patient.name)
-                && Objects.equals(lastName, patient.lastName)
-                && Objects.equals(date, patient.date)
-                && Objects.equals(PESEL, patient.PESEL);
+        User user = (User) o;
+        return Objects.equals(name, user.name) && Objects.equals(lastName, user.lastName) && Objects.equals(date, user.date) && Objects.equals(pesel, user.pesel);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, lastName, date, PESEL, age);
+        return Objects.hash(name, lastName, date, pesel);
     }
 }
+
