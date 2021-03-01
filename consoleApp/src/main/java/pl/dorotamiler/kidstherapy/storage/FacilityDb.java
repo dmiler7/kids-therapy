a11b7d37e4465ae9521bb1d21215c5d0f444a729
Dorota Miler <dorota.miler7@gmail.com>
2021-03-01 20:49:41 +0100

    Add FacilityDb storing facilities - preliminary small data base for tests

--- consoleApp/src/main/java/pl/dorotamiler/kidstherapy/storage/FacilityDb.java
+++ consoleApp/src/main/java/pl/dorotamiler/kidstherapy/storage/FacilityDb.java
@@ -1,4 +1,29 @@
 package pl.dorotamiler.kidstherapy.storage;
 
+import pl.dorotamiler.kidstherapy.domain.Address;
+import pl.dorotamiler.kidstherapy.domain.Facility;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FacilityDb {
+
+    private static List<Facility> facilities;
+
+    public static List<Facility> getAllFacilities() {
+        if (facilities.size() == 0) {
+            this.facilities = importDefaultFacilities();
+        }
+        return facilities;
+    }
+
+    private static void importDefaultFacilities() {
+        List<Facility> facilities = new ArrayList<>();
+        facilities.add(new Facility("NCM Biwakowa", new Address("Biwakowa 8", "Gdansk", 80 - 299, "587312000")));
+        facilities.add(new Facility("ODiTiS", new Address("Matemblewska 52", "Gdansk", 80 - 345, "583413020")));
+        facilities.add(new Facility("Rubikon", new Address("Portowa 4", "Gdansk", 80 - 180, "512789990")));
+        facilities.add(new Facility("Poradnia Psychologiczno-Pedagogiczna nr 1", new Address("Obywatelska 2", "Gdansk", 80 - 234, "583209871")));
+        facilities.add(new Facility("Poradnia Psychologiczno-Pedagogiczna nr 4", new Address("Obywatelska 2", "Gdynia", 80 - 003, "584559090")));
+        facilities.add(new Facility("Poradnia Psychologiczno-Pedagogiczna nr 3", new Address("Obywatelska 2", "Gdynia", 80 - 890, "583456789")));
+    }
 }
