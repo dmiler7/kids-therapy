3ab8af873935ce21458c87749fc6365b0683efc6
Dorota Miler <dorota.miler7@gmail.com>
2021-03-09 23:18:55 +0100

    Adjust Patient in lines with inheritance -> from User

--- consoleApp/src/main/java/pl/dorotamiler/kidstherapy/domain/Patient.java
+++ consoleApp/src/main/java/pl/dorotamiler/kidstherapy/domain/Patient.java
@@ -1,14 +1,13 @@
 package pl.dorotamiler.kidstherapy.domain;
 
 import java.util.Date;
-import java.util.Objects;
 
 public class Patient extends User {
 
     private Address address;
 
-    public Patient(String name, String lastName, Date date, Address address) {
-        super(name, lastName, date);
+    public Patient(String name, String lastName, Date birthDate, Address address) {
+        super(name, lastName, birthDate);
         this.address = address;
     }
 
@@ -25,23 +24,9 @@ public class Patient extends User {
         return "Patient {" +
                 "Name : " + this.getName() +
                 ", Last name : " + this.getName() +
-                ", Date : " + this.getDate() +
+                ", Date : " + this.getBirthDate() +
                 ", PESEL : " + this.getPesel() +
                 "Address : " + this.address +
                 '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        Patient patient = (Patient) o;
-        return Objects.equals(address, patient.address);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), address);
     }
 }
